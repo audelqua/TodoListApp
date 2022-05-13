f52172cf4d4e6b573186324eac1fb6c17d9610d9
audrey <mbozorgi604@gmail.com>
2022-05-13 16:44:03 +0430

    actions in list in progress

--- components/todoCard/index.tsx
+++ components/todoCard/index.tsx
@@ -3,24 +3,94 @@ import styles from './ToDoCard.module.scss'
 import { addNewTaskAction } from '../../actions'
 import { addNewTaskApi } from '../../api/todo'
 import { newTaskGenerator } from '../../helpers'
+import { FaTimes } from 'react-icons/fa';
 import TaskCell from './taskCell'
 import { useDispatch, useSelector } from 'react-redux'
 
 const ToDoCard = ({...props}) => {
-    const dispatch = useDispatch()
     const myTasks = useSelector(state => state.todoList.tasks)
-    const [note, updateNote] = useState('')
+    const [targetTask, updateTargetTask] = useState({})
+    const [editMode, updateEditMode] = useState(false)
     
+    const handleSetTargetTask = (e, task) => {
+        if(e.detail === 2) {
+            updateEditMode(task.createdAt)
+            updateTargetTask(task)
+        }
+    }
+
+    const handleSubmit = () => {
+
+    }
+
+    const handleAction = type => {
+        switch (type) {
+            case 'UPDATE':
+                console.log('UPDATE');
+                break;
+            case 'REMOVE':
+                console.log('DELETE');
+                break;
+        }
+    }
+
+    return (
+        <div className={styles.cardWrapper}>
+            <AddNewTaskForm />
+            {myTasks.length > 0 
+                ?   myTasks.map(task => 
+                        <div className={styles.listCointainer}>
+                            <div className={styles.actionWrapper}>
+                                <input type='checkbox' className={styles.checkbox} />
+                            </div>
+                            <div className={styles.taskMessageWrapper} onClick={(e) => handleSetTargetTask(e, task) }>
+                                {task.createdAt === editMode 
+                                    ?   <form className={styles.updateTaskInputSection} onSubmit={handleSubmit}>
+                                            <input 
+                                                type='text' 
+                                                className={styles.updateTaskInput} 
+                                                value={targetTask.note}
+                                                onChange={e => updateTargetTask(task => {return ({...task, note: e.target.value})})}
+                                                placeholder='What need to be done?'
+                                            />
+                                        </form>
+                                    :   <span className={styles.taskMessage}>{task.note}</span>
+                                }
+                            </div>
+                            <div 
+                                className={styles.actionWrapper + ' ' + styles.removeItem}
+                                onClick={() => handleAction(task.createdAt === editMode ? 'UPDATE' : 'REMOVE')}
+                            >
+                                {task.createdAt === editMode 
+                                    ?   <span className={styles.actionText}>Update</span>
+                                    :   <span><FaTimes /></span>
+                                }
+                            </div>
+                        </div>
+                    )
+                :   <div className={styles.noItemExist}>No task exist yet</div>
+            }
+        </div>
+    )
+}
+export default ToDoCard
+
+
+
+const AddNewTaskForm = ({...props}) => {
+    const dispatch = useDispatch()
+    const [newNote, updateNewNote] = useState('')
+
     const handleSubmit = async e => {
         e.preventDefault()
-        if(note === '') return
-        const newTask = newTaskGenerator(note)
+        if(newNote === '') return
+        const newTask = newTaskGenerator(newNote)
 
         try{
             let res = await addNewTaskApi(newTask)
             if(res["status"] === 201) {
                 dispatch(addNewTaskAction(newTask))
-                updateNote('')
+                updateNewNote('')
             }
         }catch(error) {
             console.log('error', error);
@@ -28,27 +98,19 @@ const ToDoCard = ({...props}) => {
     }
 
     return (
-        <div className={styles.cardWrapper}>
-            <form className={styles.inputSection} onSubmit={handleSubmit}>
-                <input 
-                    type='text' 
-                    className={styles.customInput} 
-                    value={note}
-                    onChange={e => updateNote(e.target.value)}
-                    placeholder='What need to be done?'
-                />
-                <div className={styles.buttonWrapper}>
-                    <button className={styles.addButton} type='submit'>
-                        <span className={styles.buttonText}>Add</span>
-                    </button>
-                </div>
-            </form>
-            {myTasks.length > 0 
-                ?   myTasks.map(task => <TaskCell task={task} /> )
-                :   <div className={styles.noItemExist}>No task exist yet</div>
-            }
-            
-        </div>
+        <form className={styles.inputSection} onSubmit={handleSubmit}>
+            <input 
+                type='text' 
+                className={styles.customInput} 
+                value={newNote}
+                onChange={e => updateNewNote(e.target.value)}
+                placeholder='What need to be done?'
+            />
+            <div className={styles.buttonWrapper}>
+                <button className={styles.addButton} type='submit'>
+                    <span className={styles.buttonText}>Add</span>
+                </button>
+            </div>
+        </form>
     )
 }
-export default ToDoCard
